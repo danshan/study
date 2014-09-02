293fdef21482f4bcf49722bef832a8d82de8692c
honghao.shan <honghao.shan@dianping.com>
2014-09-02 14:15:37 +0800

    feature add unit test demo

---
+++ unittest/README.md
@@ -0,0 +1,9 @@
+## 单元测试
+
+----
+
+主要包括两类测试方案:
+
+1. 基于内存的数据库测试 com.shanhh.testcase.demo.repository.PassportRepositoryImplTest
+
+2. 基于mock的数据库测试 com.shanhh.testcase.demo.service.PassportServiceMockTest
---
+++ unittest/src/main/java/com/shanhh/testcase/demo/dao/PassportPO.java
@@ -0,0 +1,16 @@
+package com.shanhh.testcase.demo.dao;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author dan.shan
+ * @since 2014-04-27 下午7:44
+ */
+@Data
+@ToString
+public class PassportPO {
+    int passportId;
+    String username;
+    String password;
+}
